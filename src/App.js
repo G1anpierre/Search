f7044467dc602601b130b792ddf57f4657dcb89e
Gianpierre Fernandez <gianpierre.fernandez@thinkopen.it>
2020-06-09 16:16:37 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import { makeStyles } from '@material-ui/core/styles';
+// import TextField from '@material-ui/core/TextField';
+// import Button from '@material-ui/core/Button';
+// import Grid from '@material-ui/core/Grid';
+// import Paper from '@material-ui/core/Paper';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import data from './data.json';
+import './App.scss';
+
+
+class App extends React.Component {
+
+
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+      search: ''
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      data: data.people
+    })
+  }
+
+  handleInput = (event) => {
+    this.setState({
+      [event.target.name] : event.target.value
+    })
+
+  }
+
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const filtered = data.people.filter(element => {
+      return element.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1  
+    })
+
+    console.log(filtered); 
+
+    this.setState({
+      data: filtered
+    })
+    
+  }
+
+
+  render () {
+
+    const { search, data } = this.state;
+
+    
+
+    return (
+      <div className="App">
+        <div className="container jumbotron">
+          <div className="row">
+            <div className="col-12">
+              <form onSubmit={this.handleSubmit}>
+                <div className="wrapper">
+                  <input 
+                  className="wrapper__input" 
+                  type="text"
+                  name="search"
+                  value={search}
+                  onChange={this.handleInput}/>
+                  <button 
+                  
+                  className="wrapper__button btn btn-primary"> Search </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+           <div>
+              <ul className="list-group">
+                {data.map( item => (
+                    <li key={item.id} className="list-group-item">{item.name}</li>
+                ))}
+              </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
